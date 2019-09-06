acf545ede5d0490887c26af7887f67c8cb2ec5a1
Andy <andyseymour@gambitresearch.com>
2019-09-06 10:50:22 +0100

    Updates

---
+++ precache-manifest.6ff807a030804c7081065d4820eb677a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd7fb63a47fb01be805d4539dd38276f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6f77efb6ede83a97c31",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "7fa4b32fec3ae735197e",
+    "url": "/static/js/2.7c8a6e94.chunk.js"
+  },
+  {
+    "revision": "a6f77efb6ede83a97c31",
+    "url": "/static/js/main.9712a9b3.chunk.js"
+  },
+  {
+    "revision": "3f67a8d1bccf5b99ae3a",
+    "url": "/static/js/runtime~main.401e05c4.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
