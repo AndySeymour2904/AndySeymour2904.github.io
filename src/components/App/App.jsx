b0e186a7d1e979439b918717cd395f95b54cc65e
Andy Seymour <aseymour917@gmail.com>
2020-04-12 00:38:52 +0100

    Hero page fitted to design

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,14 +1,23 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { AppBar, Toolbar, Button } from '@material-ui/core/'
+
+import Header from '../Header/Header'
 import Hero from '../Hero/Hero'
 import Experience from '../Experience/Experience'
 
-function App() {
+const useStyles = makeStyles({
+})
+
+export default function App() {
+
+  const classes= useStyles()
+
   return (
     <div>
-        <Hero />
-        <Experience />
+      <Header />
+      <Hero />
+      <Experience />
     </div>
-  );
+  )
 }
-
-export default App;
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,27 +2,25 @@ import React from 'react'
 import useMediaQuery from '@material-ui/core/useMediaQuery'
 import { makeStyles } from '@material-ui/core/styles'
 
-import { Zoom } from '@material-ui/core'
-
-import JSLogo from '../../images/logos/js.png'
-import PythonLogo from '../../images/logos/python.png'
-import DataLogo from '../../images/logos/data.png'
-
-import SocialLinks from './SocialLinks'
-
 import Typewriter from 'typewriter-effect'
+
+import Particles from 'react-particles-js'
+
+import Laptop from '../../images/laptop.png'
+
+import { AppBar, Button } from '@material-ui/core'
 
 const useStyles = makeStyles({
     root: {
         backgroundColor: '#263238',
-        height: '90vh',
+        height: '100vh',
+        display: 'flex',
     },
-    pre: {
+    preContainer: {
+        flex: 0.5,
         margin: 0,
     },
     heading: {
-        paddingTop: props => 30 - props.minusPadding + 'vh',
-        paddingBottom: '1vh',
         paddingLeft: '5vw',
         fontSize: props => props.isMaxWidth ? '96px' : '8vw',
         fontWeight: 700,
@@ -34,26 +32,26 @@ const useStyles = makeStyles({
         flexDirection: 'column',
         paddingLeft: '5vw',
         color: 'white',
-        paddingBottom: '15vh',
         fontSize: props => props.isMaxWidth ? '48px' : '4vw',
     },
-    topicHeading: {
-        color: 'white',
-        fontSize: props => props.isMaxWidth ? '36px' : '3vw',
-        display: 'flex',
-        justifyContent: 'space-around',
+    particlesContainer: {
+        position: 'absolute',
     },
-    topicContainer: {
+    laptop: {
+        width: '30vw',
+        height: '20vw',
+    },
+    imageContainer: {
+        flex: 0.5,
         display: 'flex',
-        flexDirection: 'column',
+        justifyContent: 'center'
+    },
+    heroContentContainer: {
+        width: '100vw',
+        height: '100vh',
+        display: 'flex',
         alignItems: 'center',
-        flex: 1,
-        margin: '10px',
-        maxWidth: '350px',
-    },
-    topicImage: {
-        width: '5vw',
-        marginBottom: '20px'
+        position: 'absolute',
     },
 })
 
@@ -62,97 +60,89 @@ export default function Hero() {
     const isMaxWidth = useMediaQuery('(min-width:1200px)')
 
     const [ typingStage, setTypingStage ] = React.useState(0)
-    const [ minusPadding, setMinusPadding ] = React.useState(0)
 
-    const classes= useStyles({isMaxWidth, minusPadding})
+    const classes= useStyles({isMaxWidth})
+
+    const typingStages = {
+        TITLE: 0,
+        SUBTITLE: 1,
+    }
 
     return (
         <div className={classes.root}>
-        <SocialLinks />
-        <pre className={classes.pre}>
-            <div className={classes.heading}>
-                {typingStage === 0 && (
-                    <Typewriter
-                        options={{
-                            cursor: '_'
-                        }}
-                        onInit={(typewriter) => {
-                            typewriter.pauseFor(1500)
-                            .typeString('Andy Seymour')
-                            .callFunction(() => setTypingStage(1))
-                            .start();
-                        }}
-                    />
-                )}
-                {typingStage > 0 && (
-                    <span>Andy Seymour </span>
-                )}
+            <div className={classes.particlesContainer}>
+             <Particles
+                height="100vh"
+                width="100vw"
+                params={{
+                    "particles": {
+                        "number": {
+                            "value": 50
+                        },
+                        "size": {
+                            "value": 3
+                        }
+                    },
+                    "interactivity": {
+                        "events": {
+                            "onhover": {
+                                "enable": true,
+                                "mode": "grab"
+                            }
+                        }
+                    },
+                    "retina_detect": false
+                }}
+               />
             </div>
-            <div className={classes.subheading}>
-                {typingStage === 1 && (
-                    <Typewriter
-                        options={{
-                            cursor: '_'
-                        }}
-                        onInit={(typewriter) => {
-                            typewriter.changeDelay(75)
-                            .typeString('Full stack web developer\nData engineer')
-                            .pauseFor(500)
-                            .changeDelay('natural')
-                            .callFunction(() => setMinusPadding(4))
-                            .typeString('\n')
-                            .pauseFor(0)
-                            .callFunction(() => setMinusPadding(8))
-                            .typeString('\n')
-                            .pauseFor(0)
-                            .callFunction(() => setMinusPadding(12))
-                            .typeString('\n')
-                            .pauseFor(0)
-                            .callFunction(() => setTypingStage(2))
-                            .start()
-                        }}
-                    />
-                )}
-                {typingStage > 1 && (
-                    <React.Fragment>
-                        <span>Full stack web developer</span>
-                        <span>Data engineer</span>
-                    </React.Fragment>
-                )}
+            <div className={classes.heroContentContainer}>
+                <div className={classes.preContainer}>      
+                    <pre >
+                        <div className={classes.heading}>
+                            {typingStage === typingStages.TITLE && (
+                                <Typewriter
+                                    options={{
+                                        cursor: '_'
+                                    }}
+                                    onInit={(typewriter) => {
+                                        typewriter.pauseFor(1500)
+                                        .typeString('Andy Seymour')
+                                        .callFunction(() => setTypingStage(1))
+                                        .start();
+                                    }}
+                                />
+                            )}
+                            {typingStage > typingStages.TITLE && (
+                                <span>Andy Seymour</span>
+                            )}
+                        </div>
+                        <div className={classes.subheading}>
+                            {typingStage === typingStages.SUBTITLE && (
+                                <Typewriter
+                                    options={{
+                                        cursor: '_'
+                                    }}
+                                    onInit={(typewriter) => {
+                                        typewriter.changeDelay(75)
+                                        .typeString('Full stack web developer\nData engineer')
+                                        .callFunction(() => setTypingStage(2))
+                                        .start()
+                                    }}
+                                />
+                            )}
+                            {typingStage > typingStages.SUBTITLE && (
+                                <React.Fragment>
+                                    <span>Full stack web developer</span>
+                                    <span>Data engineer</span>
+                                </React.Fragment>
+                            )}
+                        </div>
+                    </pre>
+                </div>    
+                <div className={classes.imageContainer}>      
+                    <img src={Laptop} className={classes.laptop} />
+                </div>
             </div>
-            <div className={classes.topicHeading}>
-                {typingStage > 1 && (
-                    <React.Fragment>
-                        <Zoom in>
-                            <div elevation={4} className={classes.topicContainer}>
-                                <img className={classes.topicImage} src={JSLogo} alt='JavaScript' />
-                                <span>React</span>
-                                <span>Node</span>
-                                <span>Redux</span>
-                            </div>
-                        </Zoom>
-                        <Zoom in style={{transitionDelay: '500ms'}}>
-                            <div elevation={4} className={classes.topicContainer}>
-                                <img className={classes.topicImage} src={DataLogo} alt='Data' />
-                                <span>Hadoop</span>
-                                <span>Spark</span>
-                                <span>PostgreSQL</span>
-                            </div>
-                        </Zoom>
-                        <Zoom in style={{transitionDelay: '1000ms'}}>
-                            <div elevation={4} className={classes.topicContainer}>
-                                <img className={classes.topicImage} src={PythonLogo} alt='Python' />
-                                <span>Flask</span>
-                                <span>AsyncIO</span>
-                                <span>AIOHTTP</span>
-                            </div>
-                        </Zoom>
-                    </React.Fragment>
-                )}
-            </div>
-           <button onClick={() => {setTypingStage(0); setMinusPadding(0)}}>RESET</button>
-           <button onClick={() => {setTypingStage(2); setMinusPadding(12)}}>SKIP</button>
-        </pre>
         </div>
     )
 }
